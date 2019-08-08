5adebe4a73c3da3cb14189180c24854f449847a0
hungpk205 <phankieuhunght205@gmail.com>
2019-08-08 16:33:20 +0700

    Fix create category, add index unique for name tour

--- db/migrate/20190704021340_add_default_value_parent_id.rb
+++ db/migrate/20190704021340_add_default_value_parent_id.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueParentId < ActiveRecord::Migration[5.2]
   def change
-    change_column :categories, :parent_id, :integer, default: 0
+    change_column :categories, :parent_id, :integer
   end
 end
